6d32e14937d2ab91bf4629c40cc9337b1745210e
sagar-vaghela-2869 <sagarvaghela2869@gmail.com>
2024-03-02 23:34:20 +0530

    change text on button click.

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,72 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String text = "";
+
+  void changeText(String text) {
+    setState(() {
+      this.text = text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text("HOME PAGE"),
-        backgroundColor: Colors.yellow,
+        centerTitle: false,
+        title: const Text("Hello World !"),
+        backgroundColor: Colors.blue[500],
       ),
-      body: const Text("home page text widgit"),
+      body: Column(
+        children: <Widget>[TextInput(changeText), Text(text)],
+      ),
     );
+  }
+}
+
+class TextInput extends StatefulWidget {
+  final Function(String) callabck;
+
+  const TextInput(this.callabck, {super.key});
+
+  @override
+  State<TextInput> createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void click() {
+    widget.callabck(controller.text);
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.message_rounded),
+            labelText: "Enter Something Here...",
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.send),
+              tooltip: "Send",
+              onPressed: click,
+            )));
   }
 }
