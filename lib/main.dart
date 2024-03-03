1e05ae538db28ba806082f6de4842de73bd608b9
sagar-vaghela-2869 <sagarvaghela2869@gmail.com>
2024-03-03 15:53:42 +0530

    listview with like button and count.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MainApp());
 }
 
+class Post {
+  late String body;
+  late String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    userLiked = !userLiked;
+    if (userLiked) {
+      likes += 1;
+    } else {
+      likes -= 1;
+    }
+  }
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -12,7 +30,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "Hello World",
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const HomePage(),
@@ -28,11 +46,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPost(String text) {
     setState(() {
-      this.text = text;
+      posts.add(Post(text, "Azad"));
     });
   }
 
@@ -45,7 +63,10 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.blue[500],
       ),
       body: Column(
-        children: <Widget>[TextInput(changeText), Text(text)],
+        children: <Widget>[
+          Expanded(child: PostList(posts)),
+          TextInput(newPost)
+        ],
       ),
     );
   }
@@ -71,6 +92,7 @@ class _TextInputState extends State<TextInput> {
   }
 
   void click() {
+    FocusScope.of(context).unfocus();
     widget.callabck(controller.text);
     controller.clear();
   }
@@ -80,12 +102,66 @@ class _TextInputState extends State<TextInput> {
     return TextField(
         controller: controller,
         decoration: InputDecoration(
-            prefixIcon: const Icon(Icons.message_rounded),
-            labelText: "Enter Something Here...",
+            prefixIcon: const Icon(Icons.sms_rounded),
+            labelText: "Type Something Here...",
             suffixIcon: IconButton(
-              icon: const Icon(Icons.send),
+              icon: const Icon(Icons.send_rounded),
               tooltip: "Send",
               onPressed: click,
             )));
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+  const PostList(this.listItems, {super.key});
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callBack) {
+    setState(() {
+      callBack();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = widget.listItems[index];
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  title: Text(post.body),
+                  subtitle: Text(post.author),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    child: Text(
+                      post.likes.toString(),
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.thumb_up_rounded),
+                    onPressed: () => like(post.likePost),
+                    color: post.userLiked ? Colors.blue[500] : Colors.black,
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
